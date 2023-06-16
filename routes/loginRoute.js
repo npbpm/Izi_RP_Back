44beb7b50a31b45ce64a1de985449410cce0f6b7
PEREZ BURBANO <nicolas.perez1@hotmail.com>
2023-06-16 11:26:44 +0200

    still

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -29,6 +29,8 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
+    console.log("IM USING THE LOGIN ROUTE");
+
     const { mail, password } = req.body;
 
     try {
